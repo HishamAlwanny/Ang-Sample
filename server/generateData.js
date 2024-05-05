06e045a89cf7125ef66d7db0c653f4bbf45996e9
Hisham Alwanni <hisham.alwanny@hotmail.com>
2024-05-05 19:08:24 +0300

    Fix faker random number

--- server/generateData.js
+++ server/generateData.js
@@ -1,16 +1,16 @@
-var faker = require('faker');
+var { faker } = require('@faker-js/faker');
 
-var database = { products: []};
-
-for (var i = 1; i<= 300; i++) {
-  database.products.push({
-    id: i,
-    name: faker.commerce.productName(),
-    description: faker.lorem.sentences(),
-    price: faker.commerce.price(),
-    imageUrl: "https://source.unsplash.com/1600x900/?product",
-    quantity: faker.random.number()
-  });
+var database = { products: [] };
+//faker.seed(123);
+for (var i = 1; i <= 300; i++) {
+    database.products.push({
+        id: i,
+        name: faker.commerce.productName(),
+        description: faker.lorem.sentences(),
+        price: faker.commerce.price(),
+        imageUrl: "https://source.unsplash.com/1600x900/?product",
+        quantity: faker.number.int(999)
+    });
 }
 
 console.log(JSON.stringify(database));
