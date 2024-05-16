999e2f1b14d74c88efed253115c996220558f679
Hisham Alwanni <hisham.alwanny@hotmail.com>
2024-05-16 19:37:10 +0300

    Fix HttpClientModule import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
@@ -29,6 +30,7 @@ import { AppRoutingModule } from './app-routing.module';
       { path: 'crud', component: PopoComponent}
     ]),
     BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   declarations: [
--- src/app/crud/crud.module.ts
+++ src/app/crud/crud.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { CrudRoutingModule } from './crud-routing.module';
@@ -20,7 +19,6 @@ import { UpdateComponent } from './update/update.component';
   imports: [
     CommonModule,
     CrudRoutingModule,
-    HttpClientModule,
     FormsModule
   ]
 //   ,
